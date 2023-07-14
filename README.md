41ee9d70535261126059be1d693aa5524f5c2384
atlinx <atlinuxtm@gmail.com>
2023-07-14 18:28:57 -0400

    Fix accidental nested repo

---
+++ README.md
@@ -0,0 +1,17 @@
+# Toast Task 🍞
+
+Todo/task tracking webapp build in Vue with a backend in Rust using a Postgres DB.
+
+# Building
+
+Make a copy of the `.env.template` file and replace items with `<angle-brackets>` with actual data.
+
+```bash
+docker-compose up --build
+```
+
+## Cleanup
+
+```bash
+docker-compose down -v
+```
---
+++ backend/src/main.rs
@@ -0,0 +1,42 @@
+#[macro_use]
+extern crate rocket;
+
+use std::env;
+
+use dotenv::dotenv;
+use rocket::figment::{
+    map,
+    value::{Map, Value},
+};
+use rocket_sync_db_pools::{database, diesel};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, World! This has changed!"
+}
+
+#[catch(503)]
+fn service_not_available() -> &'static str {
+    "Service not available..."
+}
+
+#[database("backend_db")]
+struct BackendDbConn(diesel::PgConnection);
+
+#[launch]
+fn rocket() -> _ {
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").unwrap();
+    println!("Using url {}", db_url);
+    let db: Map<_, Value> = map! {
+        "url" => db_url.into(),
+        "pool_size" => 10.into()
+    };
+    let figment = rocket::Config::figment().merge(("databases", map!["backend_db" => db]));
+
+    rocket::custom(figment)
+        .attach(BackendDbConn::fairing())
+        .register("/", catchers![service_not_available])
+        .mount("/", routes![index])
+}
